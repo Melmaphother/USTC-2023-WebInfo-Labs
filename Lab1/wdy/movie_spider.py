162df71170d93b173054a1ca49d0b98572308fab
Melmaphother <1559792558@qq.com>
2023-10-03 16:42:11 +0800

    del Movie_info.json and modified movie_spider.py(add function "run")

--- Lab1/wdy/movie_spider.py
+++ Lab1/wdy/movie_spider.py
@@ -1,30 +1,30 @@
 import os
-from urllib import request as re
 import requests
 from lxml import etree
 import json
 
 
 class Spider:
-    def get_head(self) -> dict:
-        headers = {
-            "User-Agent": "",
-            "Coockie": ""
-        }
-        return headers
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+    }
+    id_list = []
 
-    def get_id_list(self, file_path) -> list:
-        with open(file_path, "r") as f:
+    def get_head(self) -> dict:
+        return self.headers
+
+    def get_id_list(self, spider_file_path) -> list:
+        with open(spider_file_path, "r") as f:
             id_list_n = f.readlines()  # 有换行符
-            id_list = []
-            [id_list.append(line.strip()) for line in id_list_n]
-            return id_list
+            [self.id_list.append(line.strip()) for line in id_list_n]  # 无换行符
+            return self.id_list
 
 
 class Movie(Spider):
-    def __init__(self, file_path):
-        self.urls = self.get_id_list(file_path)
-        self.file_path = file_path
+    def __init__(self, movie_path):
+        self.urls = self.get_id_list(movie_path)
+        self.movie_path = movie_path
 
     def get_html(self, url):
         response = requests.get(url=url, headers=self.get_head())
@@ -37,11 +37,15 @@ class Movie(Spider):
         pass
 
     def save_html_to_json(self, info):
-        save_path = self.file_path[:self.file_path.rfind('/')] + '/Movie_info.json'
+        save_path = self.movie_path[0:self.movie_path.rfind('/')] + '/Movie_info.json'
         with open(save_path, 'w') as f:
             json.dump(info, f, indent=4)
 
+    def run(self):
+        pass
+
 
 if __name__ == '__main__':
-    file_path = 'aaa'
-    movie_spider = Movie(file_path)
+    movie_path = '../Dataset/Movie_id.csv'
+    movie_spider = Movie(movie_path)
+
