ac69677629f1ed377fb420524d365de764c4e627
Melmaphother <1559792558@qq.com>
2023-10-03 17:49:33 +0800

    confirm base structure in the class 'Movie'

--- Lab1/wdy/movie_spider.py
+++ Lab1/wdy/movie_spider.py
@@ -1,10 +1,12 @@
 import os
+import time
+
 import requests
-from lxml import etree
 import json
 
 
 class Spider:
+    prefix_url = 'https://movie.douban.com/subject/{}/'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                  AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
@@ -20,32 +22,60 @@ class Spider:
             [self.id_list.append(line.strip()) for line in id_list_n]  # 无换行符
             return self.id_list
 
-
-class Movie(Spider):
-    def __init__(self, movie_path):
-        self.urls = self.get_id_list(movie_path)
-        self.movie_path = movie_path
+    def get_url(self, spider_id):
+        return self.prefix_url.format(spider_id)
 
     def get_html(self, url):
         response = requests.get(url=url, headers=self.get_head())
-        content = response.content.decode('UTF-8')
-        return etree.HTML(content)
+        text = response.text
+        status_code = response.status_code
+        return text, status_code
 
-    def parse_html(self, html) -> dict:
-        # 这里应当返回一个字典 info ，包含了需要爬取的有效信息，结构应当为
+
+class Movie(Spider):
+    error = []
+    info = {
+
+    }
+
+    def __init__(self, movie_path):
+        self.movie_id_list = self.get_id_list(movie_path)
+        self.movie_path = movie_path
+
+    def parse_text(self, text) -> dict:
+        # 这里应当维护一个字典 info ，包含了需要爬取的有效信息，结构应当为
         # {'name': ' ', 'type': ' ', 'director': ' ', 'characters': ['', '', '', ...], ...}
         pass
 
-    def save_html_to_json(self, info):
-        save_path = self.movie_path[0:self.movie_path.rfind('/')] + '/Movie_info.json'
+    def save_info_to_json(self):
+        index = self.movie_path.rfind('/')
+        save_path = self.movie_path[0:index] + '/Movie_info.json' if index != -1 else 'Movie_info.json'
+        with open(save_path, 'a+') as f:
+            json.dump(self.info, f, indent=4)
+
+    def save_error_message(self):
+        index = self.movie_path.rfind('/')
+        save_path = self.movie_path[0:index] + '/Movie_error.txt' if index != -1 else 'Movie_error.txt'
         with open(save_path, 'w') as f:
-            json.dump(info, f, indent=4)
+            f.writelines(self.error)
 
     def run(self):
-        pass
+        for movie_id in self.movie_id_list:
+            print("正在爬取id为 {} 的电影的信息\n".format(movie_id))
+            movie_url = self.get_url(movie_id)
+            text, status_code = self.get_html(movie_url)
+            if status_code == 404:
+                print("id：{} 的资源不存在！\n".format(movie_id))
+                self.error.append(movie_id)
+            else:
+                self.parse_text(text)
+                self.save_info_to_json()
+
+            time.sleep(2)  # 休眠 2s
+
+        self.save_error_message()
 
 
 if __name__ == '__main__':
     movie_path = '../Dataset/Movie_id.csv'
     movie_spider = Movie(movie_path)
-
