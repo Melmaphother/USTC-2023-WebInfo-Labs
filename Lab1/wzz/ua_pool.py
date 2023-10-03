4effd70c34e721fed76ddd4635b1054b145ffdbf
Melmaphother <1559792558@qq.com>
2023-10-03 17:50:17 +0800

    modify some errors(lack of self) in ua_pool.py

--- Lab1/wzz/ua_pool.py
+++ Lab1/wzz/ua_pool.py
@@ -1,32 +1,33 @@
 from fake_useragent import UserAgent
 import random
 
-class uapool:
-    count = 0
-    uapool = []
-    def add_ua_to_pool(times):
-        rand = random.randrange(1,4,1)
-        for i in range(times + 1):
-            if  rand == 1 :
-                self.uapool.append(ua.chrome)
-            elif rand == 2:
-                self.uapool.append(ua.firefox)
-            elif rand == 3:
-                self.uapool.append(ua.edge)
-            else:
-                uapool.append(ua.safari)
 
-    def __init__ ():
-        ua=UserAgent()
-        times = random.randrange(5,15,1)
-        add_ua_to_pool(times)
+class UAPool:
+    count = 0
+    ua_pool = []
+
+    def add_ua_to_pool(self, times):
+        rand = random.randrange(1, 4, 1)
+        for i in range(times + 1):
+            if rand == 1:
+                self.ua_pool.append(self.ua.chrome)
+            elif rand == 2:
+                self.ua_pool.append(self.ua.firefox)
+            elif rand == 3:
+                self.ua_pool.append(self.ua.edge)
+            else:
+                self.ua_pool.append(self.ua.safari)
+
+    def __init__(self):
+        self.ua = UserAgent()
+        times = random.randrange(5, 15, 1)
+        self.add_ua_to_pool(times)
         self.count = times
-        
-    def pop():
+
+    def pop(self):
         if self.count >= 1:
             self.count -= 1
         else:
-            add_ua_to_pool(10)
+            self.add_ua_to_pool(10)
             self.count += 9
-        return self.uapool.pop()
-
+        return self.ua_pool.pop()
