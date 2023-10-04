b439b10613a460bd30e5d74d1438f0f4d7f7bfe8
Melmaphother <1559792558@qq.com>
2023-10-04 15:09:44 +0800

    finish movie_spider.py

--- Lab1/wdy/movie_spider.py
+++ Lab1/wdy/movie_spider.py
@@ -1,4 +1,4 @@
-import os
+import re
 import time
 from bs4 import BeautifulSoup
 import requests
@@ -8,8 +8,8 @@ import json
 class Spider:
     prefix_url = 'https://movie.douban.com/subject/{}/'
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
     }
     id_list = []
 
@@ -33,44 +33,62 @@ class Spider:
 
 
 class Movie(Spider):
-    error = []
-    info = {
-        "name": "",
-        "type": "",
-        "director": "",
-        "characters": {},
-        "country_or_region": "",
-        "language": "",
-        "release_date"
-        "film_length": "",
-        "alias": {},
-        "IMDb": "",
-        "intro": ""
-    }
 
     def __init__(self, movie_path):
         self.movie_id_list = self.get_id_list(movie_path)
         self.movie_path = movie_path
+        self.error = []
+        self.info = {}
 
     def parse_text(self, text, movie_id):
         # 这里应当维护一个字典 info ，包含了需要爬取的有效信息，结构应当为
         # {'name': ' ', 'type': ' ', 'director': ' ', 'characters': ['', '', '', ...], ...}
         soup = BeautifulSoup(text, 'html.parser')
+        """
+            提取影片名
+        """
         name = soup.find('span', {'property': 'v:itemreviewed'})
         if name is None:
             print(movie_id, "没有名称\n")
             return None
         self.info['name'] = name.text
+        """
+            提取包括导演，主演等信息
+        """
         main_info = soup.find('div', {'id': 'info'})
         if main_info is None:
             print(movie_id, "没有导演等主要信息\n")
             return None
-        # TODO
+        match_list = {'导演: ': '', '编剧: ': '', '主演: ': '', '类型: ': '', '制片国家/地区: ': '', '语言: ': '',
+                      '上映日期: ': '', '片长: ': '', '又名: ': '', 'IMDb: ': ''}
+
+        for item in match_list.keys():
+            match = re.search(item + r'(.*)$', main_info, re.M)  # re.M 表示用行匹配
+            if match:
+                match_list[item] = match.group(1)
+            else:
+                print(movie_id, "中'", item, "'没有对应匹配\n")
+                return None
+        self.info['director'] = match_list['导演: '].split(' / ')
+        self.info['characters'] = match_list['主演: '].split(' / ')
+        self.info['playwright'] = match_list['编剧: '].split(' / ')
+        self.info['type'] = match_list['类型: '].split(' / ')
+        self.info['country_or_region'] = match_list['制片国家/地区: '].split(' / ')
+        self.info['language'] = match_list['语言: '].split(' / ')
+        self.info['release_date'] = match_list['上映日期: ']
+        self.info['film_length'] = match_list['片长']
+        self.info['alias'] = match_list['又名: '].split(' / ')
+        self.info['IMDb'] = match_list['IMDb: ']
+        """
+            提取简要介绍
+        """
         intro = soup.find('span', {'class': "all hidden"})
         if intro is None:
             print(movie_id, "没有介绍\n")
             return None
-        self.info[intro] = intro.text
+        intro_text = intro.text.replace('\n', '').replace('\r', '')  # Windows下两个要同时去除
+        intro_text = intro_text.replace('　', '').replace(' ', '')  # 注意前面是全角空格，后面是半角空格
+        self.info[intro] = intro_text
 
     def save_info_to_json(self, movie_id):
         index = self.movie_path.rfind('/')
@@ -97,7 +115,6 @@ class Movie(Spider):
                 self.save_info_to_json()
 
             time.sleep(2)  # 休眠 2s
-
         self.save_error_message()
 
 
