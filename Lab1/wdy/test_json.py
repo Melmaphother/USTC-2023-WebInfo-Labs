e6eb63038948a533733f5c86d399d7a979f66cc5
Melmaphother <1559792558@qq.com>
2023-10-06 04:58:30 +0800

    modify test_json.py

--- Lab1/wdy/test_json.py
+++ Lab1/wdy/test_json.py
@@ -1,36 +1,23 @@
 import json
 
-
-with open('../Dataset/Movie_info1.json', 'r', encoding='UTF-8') as f:
+save_path = '../Result/Movie_info.json'
+with open(save_path, 'r', encoding='UTF-8') as f:
     json_data = json.load(f)
 
-with open('../Dataset/Movie_id.csv', "r") as f:
-    id_list = []
-    id_list_n = f.readlines()  # 有换行符
-    [id_list.append(line.strip()) for line in id_list_n]  # 无换行符
+tag_path = '../Dataset/Tag/Movie_tag.csv'
+with open(tag_path, 'r', encoding='UTF-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip('\n')
+        index = line.find(',')
+        if index != -1:
+            movie_id = line[0:index]
+            tag = line[index + 1:]
+            print(movie_id, ' ', tag)
+        if movie_id not in json_data.keys():
+            continue
+        json_data[movie_id]['tag'] = tag.strip('"').split(',')
 
-
-json_list = list(json_data.keys())
-for item in id_list:
-    if item not in json_list:
-        print(item)
-
-
-# 1309046
-# 1768351
-# 1293691
-# 1292790
-# 1316505
-# 1292331
-# 1796939
-# 1308988
-# 1307528
-# 1303053
-# 1307072
-# 2043126
-# 1305088
-# 1296935
-# 1304900
-# 1829626
-
-
+save_path_1 = '../Result/Movie_info1.json'
+with open(save_path_1, 'w', encoding='UTF-8') as f:
+    json.dump(json_data, f, indent=4, ensure_ascii=False)
