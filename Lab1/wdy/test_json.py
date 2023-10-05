af4e0553bf4e1d9e8255a911bb7316bc1178d8fc
Melmaphother <1559792558@qq.com>
2023-10-06 05:01:45 +0800

    modify test_info.json

--- Lab1/wdy/test_json.py
+++ Lab1/wdy/test_json.py
@@ -1,10 +1,10 @@
 import json
 
-save_path = '../Result/Movie_info.json'
+save_path = '../Result/Book_info.json'
 with open(save_path, 'r', encoding='UTF-8') as f:
     json_data = json.load(f)
 
-tag_path = '../Dataset/Tag/Movie_tag.csv'
+tag_path = '../Dataset/Tag/Book_tag.csv'
 with open(tag_path, 'r', encoding='UTF-8') as f:
     lines = f.readlines()
     for line in lines:
@@ -18,6 +18,6 @@ with open(tag_path, 'r', encoding='UTF-8') as f:
             continue
         json_data[movie_id]['tag'] = tag.strip('"').split(',')
 
-save_path_1 = '../Result/Movie_info1.json'
+save_path_1 = '../Result/Book_info1.json'
 with open(save_path_1, 'w', encoding='UTF-8') as f:
     json.dump(json_data, f, indent=4, ensure_ascii=False)
