6562d81827b12fb5a388dab5eb63019725c0c719
Melmaphother <1559792558@qq.com>
2023-10-14 12:23:30 +0800

    Init Search.py

---
+++ Lab1/wdy/Stage1_2/Search.py
@@ -0,0 +1,37 @@
+from typing import AnyStr, List
+
+
+class Search:
+    def __init__(self):
+        pass
+
+    def Search(self):
+        pass
+
+    def E(self, e: str) -> list:
+        pass
+
+    def F(self, f: str) -> list:
+        pass
+
+    def T(self, t: str) -> list:
+        pass
+
+    def G(self, g: str) -> list:
+        pass
+
+
+class Operation:
+
+    def OR(self, L1: list, L2: List) -> list:
+        pass
+
+    def AND(self, L1: list, L2: list) -> list:
+        pass
+
+    def NOT(self, L1: list, L2: list) -> list:
+        pass
+
+
+if __name__ == '__main__':
+    pass
