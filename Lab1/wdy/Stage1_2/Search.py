37b5041415cd76e3bb6d0cb14816141b5838d591
Melmaphother <1559792558@qq.com>
2023-10-18 18:38:40 +0800

    add AND NOT and NOT NOT

--- Lab1/wdy/Stage1_2/Search.py
+++ Lab1/wdy/Stage1_2/Search.py
@@ -1,6 +1,8 @@
 from typing import AnyStr, List
 import json
 
+output_len = 80
+
 
 class BooleanMatch:
     def __init__(self):
@@ -12,11 +14,12 @@ class BooleanMatch:
         self.info_mirror = {}
         self.inverted_table_mirror = {}
         # Load data
-        print("LOADING DATA!\nPlease wait for a few seconds!")
+        print("Douban Searching Engine\n".center(output_len))
+        print("LOADING DATA! Please wait for a few seconds!\n".center(output_len))
         self.book_info_path = '../../Stage1_1/Result/Book_info.json'
         self.movie_info_path = '../../Stage1_1/Result/Movie_info.json'
-        self.book_inverted_table_path = '../../wzz/Stage1_2/inverted_list_temp.json'
-        self.movie_inverted_table_path = '../../wzz/Stage1_2/inverted_list_temp.json'
+        self.book_inverted_table_path = '../../wzz/Stage1_2/data/reverted_dict_temp.json'
+        self.movie_inverted_table_path = '../../wzz/Stage1_2/data/reverted_dict_temp.json'
         with open(self.book_info_path, 'r', encoding="utf-8") as f_book_info:
             self.book_info = json.load(f_book_info)
 
@@ -29,9 +32,9 @@ class BooleanMatch:
         with open(self.movie_inverted_table_path, 'r', encoding="utf-8") as f_movie_inverted_table:
             self.movie_inverted_table = json.load(f_movie_inverted_table)
 
-        print("Initialization completed! Please start retrieval.")
+        print("Initialization completed! Start you travel!\n".center(output_len))
 
-    def SplitQuery(self) -> list:
+    def SplitQuery(self) -> List:
         self.query = self.query.strip()
         self.query = self.query.replace('（', '(').replace('）', ')')
         self.query = self.query.replace('(', ' ( ').replace(')', ' ) ')
@@ -45,7 +48,7 @@ class BooleanMatch:
         flag = 0
         while i < len(self.query_list) and not self.error:
             if flag < 0:
-                print("The right bracket overabundant!")
+                print("The right bracket overabundant!\n".center(output_len))
                 self.error = True
             elif self.query_list[i] == ')':
                 if flag == 0:
@@ -55,7 +58,7 @@ class BooleanMatch:
             elif self.query_list[i] == '(':
                 flag += 1
             i += 1
-        print("Lack of right bracket!")
+        print("Lack of right bracket!\n".center(output_len))
         self.error = True
         return -1
 
@@ -66,18 +69,14 @@ class BooleanMatch:
         self.info_mirror = {'book': self.book_info, 'movie': self.movie_info}
         self.inverted_table_mirror = {'book': self.book_inverted_table, 'movie': self.movie_inverted_table}
         ret = self.BracketOperation(self.query_list)
-        if len(ret) != 1:
-            print("There are some unexpected words")
-            self.error = True
+        if len(ret) == 0:
+            print("Sorry! But there are no results you want here.\n".center(output_len))
+            # not find doesn't mean error
         if not self.error:
-            expect_id_list = ret[0]
-            if not expect_id_list:
-                print("There are no results you want here. Search for something else?")
-            else:
-                print(expect_id_list)
+            print(ret)
         return self.error
 
-    def BracketOperation(self, query_list) -> list:
+    def BracketOperation(self, query_list) -> List:
         if not query_list:
             return []
         ret = []
@@ -91,41 +90,60 @@ class BooleanMatch:
                     if not self.error:
                         ret.append(self.BracketOperation(query_list[l_bracket + 1: r_bracket]))
                         index = r_bracket + 1
+                    else:
+                        index += 1
                 elif item == 'AND' or item == 'OR' or item == 'NOT':
                     ret.append(item)
+                    index += 1
                 else:
                     inverted_table = self.inverted_table_mirror[self.mode]
                     ret.append(inverted_table[item] if item in inverted_table.keys() else [])
-                index += 1
+                    index += 1
             else:
                 break
-        return self.LogicOperation(ret)
+        logic_ret = self.LogicOperation(ret)
+        return logic_ret
 
-    def LogicOperation(self, ret: list) -> list:
+    def LogicOperation(self, ret: list) -> List:
         if 'OR' in ret:
             or_list = []
             for loc, val in enumerate(ret):
                 if val == 'OR':
                     or_list.append(loc)
             last_or_index = or_list[-1]
-            return self.OR(self.LogicOperation(ret[: last_or_index]), ret[last_or_index + 1])
+            return self.OR(self.LogicOperation(ret[: last_or_index]), self.LogicOperation(ret[last_or_index + 1:]))
         elif 'AND' in ret:
             and_list = []
             for loc, val in enumerate(ret):
                 if val == 'AND':
                     and_list.append(loc)
             last_and_index = and_list[-1]
-            return self.AND(self.LogicOperation(ret[: last_and_index]), ret[last_and_index + 1])
+            if ret[last_and_index + 1] == 'NOT':
+                if ret[last_and_index + 2] != 'NOT':
+                    return self.AND_NOT(self.LogicOperation(ret[: last_and_index]),
+                                        self.LogicOperation(ret[last_and_index + 2:]))
+            return self.AND(self.LogicOperation(ret[: last_and_index]),
+                            self.LogicOperation(ret[last_and_index + 1:]))
         elif 'NOT' in ret:
             first_not_index = ret.index('NOT')
-            return self.NOT(self.LogicOperation(ret[first_not_index + 1:]))
+            if ret[first_not_index + 1] == 'NOT':
+                if ret[first_not_index + 2] != 'NOT':
+                    return self.LogicOperation(ret[first_not_index + 2:])
+            else:
+                return self.NOT(self.LogicOperation(ret[first_not_index + 1:]))
         else:
+            if not self.error and len(ret) == 0:
+                print("Lack of some parameters\n".center(output_len))
+                self.error = True
+            elif not self.error and len(ret) > 1:
+                print("There are some unexpected parameters\n".center(output_len))
+                self.error = True
             return ret[0]
 
-    def OR(self, L1: List, L2: List) -> list:
+    def OR(self, L1: List, L2: List) -> List:
         ret = []
         if not L1 or not L2:
-            print("The operand 'OR' lacks parameter!")
+            print("The operand 'OR' lacks parameter!\n".center(output_len))
             self.error = True
         if not self.error:
             index1 = 0
@@ -147,10 +165,10 @@ class BooleanMatch:
                 ret.append(L2[index2:])
         return ret
 
-    def AND(self, L1: List, L2: List) -> list:
+    def AND(self, L1: List, L2: List) -> List:
         ret = []
         if not L1 or not L2:
-            print("The operand 'AND' lacks parameter!")
+            print("The operand 'AND' lacks parameter!\n".center(output_len))
             self.error = True
         if not self.error:
             index1 = 0
@@ -166,14 +184,28 @@ class BooleanMatch:
                     index2 += 1
         return ret
 
+    def AND_NOT(self, L1: List, L2: List) -> List:
+        ret = []
+        if not L2:
+            print("The operand 'NOT' lacks parameter!\n".center(output_len))
+            self.error = True
+        if not self.error:
+            for x in L1:
+                if x not in L2:
+                    ret.append(x)
+        return ret
+
     def NOT(self, L: List) -> list:
         ret = []
         if not L:
-            print("The operand 'NOT' lacks parameter!")
+            print("The operand 'NOT' lacks parameter!\n".center(output_len))
             self.error = True
         if not self.error:
             info = self.info_mirror[self.mode]
-            ret = [x for x in info.key() not in L]
+            for x in info.keys():
+                if x not in L:
+                    ret.append(x)
+        ret.sort()
         return ret
 
 
@@ -182,16 +214,23 @@ if __name__ == '__main__':
     while True:
         while True:
             mode = input(
-                "Please input which mode you'll search: book / movie?\n")
+                "Please input which mode you'll search: book / movie?\n".center(output_len))
             if mode == 'book' or mode == 'movie':
                 break
             else:
-                print("Some error? Please input the mode again!")
-        query = input("Please input the sequence you'll search:\n")
+                print("Some error! Please be care that you can only choose 'book' or 'movie'!\n".center(output_len))
+
+        query = input("Please input the sequence you'll search:\n".center(output_len))
+
         error = bm.BooleanSearch(query, mode)
+
         if error:
-            print("Your given sequence is error, Please input again!\n")
-        next_choice = input("Would you still like to search: y / n?\n")
+            next_choice = input("Maybe search for something else? y / n?\n".center(output_len))
+        else:
+            next_choice = input("Would you still like to search: y / n?\n".center(output_len))
+
         if next_choice == 'n':
-            print("Thank you for using this searching engine!")
+            print("Thank you for using this searching engine! Welcome your next travel!".center(output_len))
             break
+
+# （一部）And Not NOt动人
