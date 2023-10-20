bcdf0d92e040abc7aefb64e8bcc49941e5dd3397
Wloner0809 <wloner@mail.ustc.edu.cn>
2023-10-20 18:19:13 +0800

    split class

--- Lab1/wy/Stage1_2/split_word.py
+++ Lab1/wy/Stage1_2/split_word.py
@@ -37,25 +37,27 @@ class Split:
 
     # can choose two modes to split the words
     def split_info(self, text: str, mode="jieba") -> List:
-        pattern = '[^A-Za-z0-9\u4e00-\u9fa5]'
+        # pattern = '[^A-Za-z0-9\u4e00-\u9fa5]'
         if mode == "jieba":
-            seg_list = jieba.lcut(re.sub(pattern, '', text), cut_all=False)
+            # seg_list = jieba.lcut(re.sub(pattern, '', text), cut_all=False)
+            seg_list = jieba.lcut(text, cut_all=False)
         else:
-            seg_list = pkuseg.pkuseg().cut(re.sub(pattern, '', text))
+            # seg_list = pkuseg.pkuseg().cut(re.sub(pattern, '', text))
+            seg_list = pkuseg.pkuseg().cut(text)
         extracted_word = []
         print(seg_list)
         for word in seg_list:
             if word not in self.stop_word_list:
                 extracted_word.append(word)
         for i in range(len(extracted_word)):
-            if extracted_word[i] not in self.single_id_info and extracted_word[i] != '':
+            if extracted_word[i] not in self.single_id_info and extracted_word[i] != ' ':
                 self.single_id_info.append(extracted_word[i])
             for j in range(i + 1, len(extracted_word)):
-                if len(extracted_word[j]) > 0 and len(extracted_word[i]) > 0:
+                if len(extracted_word[j]) > 0 and len(extracted_word[i]) > 0 and extracted_word[i] != ' ' and extracted_word[j] != ' ':
                     if extracted_word[j] not in self.single_id_info and synonyms.compare(extracted_word[i],
                                                                                          extracted_word[j],
                                                                                          seg=False) > 0.6:
-                        extracted_word[j] = ''
+                        extracted_word[j] = ' '
         return self.single_id_info
 
     def combine_single_info(self, id__: str) -> Dict:
@@ -66,21 +68,3 @@ class Split:
     def save_keyword_to_json(self):
         with open(self.result_path, 'w', encoding="UTF-8") as f:
             json.dump(self.extracted_info, f, indent=4, ensure_ascii=False)
-
-
-if __name__ == "__main__":
-    path1 = "./Result/Book_info.json"
-    path2 = "./Dataset/cn_stopwords.txt"
-    path3 = "./Dataset/Book_id.csv"
-    path4 = "./Result/Book_keyword.json"
-    book_test = Split(path3, path1, path2, path4)
-    book_test.get_id_list()
-    book_test.get_full_info()
-    book_test.get_stop_word_list()
-    for id_ in book_test.id_list:
-        book_test.split_info(book_test.full_info[id_]['title'], "pku")
-        book_test.split_info(book_test.full_info[id_]['author introduction'], "pku")
-        book_test.split_info(book_test.full_info[id_]['content introduction'], "pku")
-        print(book_test.single_id_info)
-        book_test.combine_single_info(id_)
-    book_test.save_keyword_to_json()
