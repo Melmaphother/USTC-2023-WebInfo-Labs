142acb02505a87a500d64093854f07b4e330f5c1
Melmaphother <1559792558@qq.com>
2023-10-27 20:45:19 +0800

    modify compress and decompress

--- Lab1/wzz/Stage1_2/compress.py
+++ Lab1/wzz/Stage1_2/compress.py
@@ -1,34 +1,37 @@
 import json
-def compress(index_list)->bytes:
-    count = 0
-    vb_arr = []                                     #可变长度编码对应的列表（最后转换成bytes类）
-    prev = len(index_list)-2
 
-    for i in range(len(index_list)-1,0,-1):         #用文档间距替代id
+
+def compress(index_list) -> bytes:
+    count = 0
+    vb_arr = []  # 可变长度编码对应的列表（最后转换成bytes类）
+    prev = len(index_list) - 2
+
+    for i in range(len(index_list) - 1, 0, -1):  # 用文档间距替代id
         index_list[i] = index_list[i] - index_list[prev]
-        prev -=1
-        
-    for i in range(0,len(index_list)):            #index_list:若干个长的id
+        prev -= 1
+
+    for i in range(0, len(index_list)):  # index_list:若干个长的id
         reverted_arr = []
-        while index_list[i] >= 128 :
-            mod = index_list[i]%128
+        while index_list[i] >= 128:
+            mod = index_list[i] % 128
             reverted_arr.append(mod)
             index_list[i] = index_list[i] >> 7
         if index_list[i] > 0:
             reverted_arr.append(index_list[i])
-        reverted_arr[0] = int(reverted_arr[0])+128
-        for j in range(len(reverted_arr)-1,-1,-1):           #倒着遍历
+        reverted_arr[0] = int(reverted_arr[0]) + 128
+        for j in range(len(reverted_arr) - 1, -1, -1):  # 倒着遍历
             vb_arr.append(reverted_arr[j])
         pass
     result = bytes(vb_arr)
     return result
 
+
 if __name__ == "__main__":
-    with open(r"D:\web_lab\WebInfo\Lab1\wzz\Stage1_2\data\Book_reverted_dict.json","r",encoding="UTF-8" ) as f_in:
+    with open("data/Movie_reverted_dict.json", "r", encoding="UTF-8") as f_in:
         reverted_dict = json.load(f_in)
-        
-    with open(r"D:\web_lab\WebInfo\Lab1\wzz\Stage1_2\data\Book_reverted_dict_compressed.bin","wb") as f_bin:
-        with open(r"D:\web_lab\WebInfo\Lab1\wzz\Stage1_2\data\Book_vacabulary.txt","w",encoding="UTF-8") as f_txt:
+
+    with open("data/Movie_reverted_dict_compressed.bin", "wb") as f_bin:
+        with open("data/Movie_vocabulary.txt", "w", encoding="UTF-8") as f_txt:
             for key in reverted_dict:
                 f_txt.write(key + "%" + str(len(reverted_dict[key])) + "\n")
                 reverted_dict[key] = compress(reverted_dict[key])
